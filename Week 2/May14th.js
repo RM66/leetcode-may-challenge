def801669cf7db3e656ab8c4df84388ceb0b2b54
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-05-15 11:16:44 +0500

    May 14th

--- Week 2/May14th.js
+++ Week 2/May14th.js
@@ -2,22 +2,7 @@
  * Initialize your data structure here.
  */
 var Trie = function () {
-  this.root = {
-    children: [],
-  };
-};
-
-Trie.traverse = function (node, condition) {
-  var result;
-  for (var child of node.children) {
-    if (condition(child)) {
-      result = child;
-      break;
-    }
-    result = Trie.traverse(child, condition);
-    if (result) break;
-  }
-  return result;
+  this.root = {};
 };
 
 /**
@@ -26,34 +11,12 @@ Trie.traverse = function (node, condition) {
  * @return {void}
  */
 Trie.prototype.insert = function (word) {
-  var parent;
-
-  if (!this.root.children.length) parent = this.root;
-  else {
-    if (this.search(word)) return;
-    var node = Trie.traverse(this.root, (n) => n.value.startsWith(word));
-    if (node) {
-      var parent = node.parent;
-      parent.children.splice(
-        parent.children.findIndex((n) => n === node),
-        1
-      );
-      var newN = { value: word, parent: parent, children: [node] };
-      node.parent = newN;
-      parent.children.push(newN);
-      return;
-    }
-    var str = word;
-    while (str.length > 1) {
-      str = str.slice(0, -1);
-      node = Trie.traverse(this.root, (n) => n.value === str);
-      if (node) break;
-    }
-    parent = node;
+  var node = this.root;
+  for (var ch of word) {
+    if (!node[ch]) node[ch] = {};
+    node = node[ch];
   }
-  if (!parent) parent = this.root;
-
-  parent.children.push({ value: word, parent: parent, children: [] });
+  node.isWord = true;
 };
 
 /**
@@ -62,7 +25,12 @@ Trie.prototype.insert = function (word) {
  * @return {boolean}
  */
 Trie.prototype.search = function (word) {
-  return !!Trie.traverse(this.root, (n) => n.value === word);
+  var node = this.root;
+  for (var ch of word) {
+    if (!node[ch]) return false;
+    node = node[ch];
+  }
+  return !!node.isWord;
 };
 
 /**
@@ -71,20 +39,17 @@ Trie.prototype.search = function (word) {
  * @return {boolean}
  */
 Trie.prototype.startsWith = function (prefix) {
-  return !!Trie.traverse(this.root, (n) => n.value.startsWith(prefix));
+  var node = this.root;
+  for (var ch of prefix) {
+    if (!node[ch]) return false;
+    node = node[ch];
+  }
+  return true;
 };
-
-/**
- * Your Trie object will be instantiated and called as such:
- * var obj = new Trie()
- * obj.insert(word)
- * var param_2 = obj.search(word)
- * var param_3 = obj.startsWith(prefix)
- */
 
 /*
 15 / 15 test cases passed.
 Status: Accepted
-Runtime: 5024 ms
-Memory Usage: 52.6 MB
+Runtime: 156 ms
+Memory Usage: 53.8 MB
 */

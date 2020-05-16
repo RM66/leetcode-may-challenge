81a64ac004340c649f1d65a698892fe2d58c4268
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-05-16 15:57:59 +0500

    May 15th

--- Week 3/May15th.js
+++ Week 3/May15th.js
@@ -4,25 +4,26 @@
  */
 var maxSubarraySumCircular = function (A) {
   if (A.length === 1) return A[0];
+
   var totalSum = 0;
   for (var num of A) totalSum += num;
-  var maxSum = maxArrSum(A, 1);
-  var maxNegSum = maxArrSum(A, -1);
-  return maxSum < 0 ? maxSum : Math.max(maxSum, totalSum + maxNegSum);
-};
 
-function maxArrSum(arr, sign) {
-  var max = (sum = -Infinity);
-  for (var i = 0; i < arr.length; i++) {
-    sum = Math.max(arr[i] * sign, arr[i] * sign + sum);
-    if (sum > max) max = sum;
+  var maxSum = (tempMaxSum = -Infinity);
+  var minSum = (tempMinSum = Infinity);
+
+  for (var i = 0; i < A.length; i++) {
+    tempMaxSum = Math.max(A[i], A[i] + tempMaxSum);
+    if (tempMaxSum > maxSum) maxSum = tempMaxSum;
+    tempMinSum = Math.min(A[i], A[i] + tempMinSum);
+    if (tempMinSum < minSum) minSum = tempMinSum;
   }
-  return max;
-}
+
+  return maxSum < 0 ? maxSum : Math.max(maxSum, totalSum - minSum);
+};
 
 /*
 109 / 109 test cases passed.
 Status: Accepted
-Runtime: 72 ms
-Memory Usage: 41.3 MB
+Runtime: 64 ms
+Memory Usage: 41.7 MB
 */

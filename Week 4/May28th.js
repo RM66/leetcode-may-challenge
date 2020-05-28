ca31a9dd382713e235a4d598bccd09799435b664
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-05-28 19:26:42 +0500

    May 28th

--- Week 4/May28th.js
+++ Week 4/May28th.js
@@ -16,3 +16,41 @@ Status: Accepted
 Runtime: 136 ms
 Memory Usage: 47.9 MB
 */
+
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var countBits = function (num) {
+  var res = new Array(num + 1);
+  res[0] = 0;
+  for (var n = 1; n <= num; n++) {
+    if (n % 2) res[n] = res[n - 1] + 1;
+    else res[n] = res[n / 2];
+  }
+  return res;
+};
+
+/*
+15 / 15 test cases passed.
+Status: Accepted
+Runtime: 84 ms
+Memory Usage: 41.6 MB
+*/
+
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var countBits = function (num) {
+  var res = [0];
+  for (var n = 1; n <= num; n++) res.push(res[n >> 1] + (n % 2));
+  return res;
+};
+
+/*
+15 / 15 test cases passed.
+Status: Accepted
+Runtime: 84 ms
+Memory Usage: 41.6 MB
+*/

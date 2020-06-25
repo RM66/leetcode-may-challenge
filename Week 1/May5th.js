f4f8efe8a3a3636724682b9d59f79f1a0ad0fa7a
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-06-25 15:54:30 +0500

    May 5th

--- Week 1/May5th.js
+++ Week 1/May5th.js
@@ -20,3 +20,46 @@ Status: Accepted
 Runtime: 64 ms
 Memory Usage: 37.6 MB
 */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  const counter = {};
+  for (let i = 0; i < s.length; i++) {
+    const code = s.charCodeAt(i);
+    if (code in counter) counter[code]++;
+    else counter[code] = 1;
+  }
+  for (let i = 0; i < s.length; i++) {
+    const code = s.charCodeAt(i);
+    if (counter[code] === 1) return i;
+  }
+  return -1;
+};
+
+/*
+104 / 104 test cases passed.
+Status: Accepted
+Runtime: 68 ms
+Memory Usage: 38.5 MB
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === i && s.indexOf(s[i], i + 1) === -1) return i;
+  }
+  return -1;
+};
+
+/*
+104 / 104 test cases passed.
+Status: Accepted
+Runtime: 68 ms
+Memory Usage: 38.8 MB
+*/

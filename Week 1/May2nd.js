d58bcdf55a765cd57997f98e02c76d7e5d9e3db9
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-06-25 17:03:28 +0500

    May 2nd

--- Week 1/May2nd.js
+++ Week 1/May2nd.js
@@ -14,3 +14,20 @@ Status: Accepted
 Runtime: 56 ms
 Memory Usage: 34.2 MB
 */
+
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function (J, S) {
+  var matched = S.match(new RegExp(`[${J}]`, "g"));
+  return matched ? matched.length : 0;
+};
+
+/*
+254 / 254 test cases passed.
+Status: Accepted
+Runtime: 68 ms
+Memory Usage: 34.2 MB
+*/

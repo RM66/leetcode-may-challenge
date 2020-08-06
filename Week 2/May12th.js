e64faf50fec3f1cef46123bb75955b175d9317de
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-08-06 15:51:16 +0500

    May 12th

--- Week 2/May12th.js
+++ Week 2/May12th.js
@@ -14,3 +14,25 @@ Status: Accepted
 Runtime: 44 ms
 Memory Usage: 35.6 MB
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function (nums) {
+  var left = 0;
+  var right = nums.length - 1;
+  while (left <= right) {
+    var mid = Math.trunc((left + right) / 2);
+    if (nums[mid] === nums[mid ^ 1]) left = mid + 1;
+    else right = mid - 1;
+  }
+  return nums[left];
+};
+
+/*
+14 / 14 test cases passed.
+Status: Accepted
+Runtime: 60 ms
+Memory Usage: 36.7 MB
+*/

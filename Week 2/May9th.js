4fd2b7f79636e792933576db4fc13916b16590ac
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-08-06 13:58:20 +0500

    May 9th

--- Week 2/May9th.js
+++ Week 2/May9th.js
@@ -15,3 +15,27 @@ Status: Accepted
 Runtime: 52 ms
 Memory Usage: 34.4 MB
 */
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+  var left = 0;
+  var right = num;
+  while (left <= right) {
+    var mid = Math.trunc((left + right) / 2);
+    var square = mid * mid;
+    if (square === num) return true;
+    if (square > num) right = mid - 1;
+    else left = mid + 1;
+  }
+  return false;
+};
+
+/*
+70 / 70 test cases passed.
+Status: Accepted
+Runtime: 64 ms
+Memory Usage: 36.6 MB
+*/

976ac3ded70ae948370fe01f708c8301130da843
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-08-20 17:21:40 +0500

    May 18th

--- Week 3/May18th.js
+++ Week 3/May18th.js
@@ -39,3 +39,43 @@ Status: Accepted
 Runtime: 80 ms
 Memory Usage: 41.8 MB
 */
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {
+  if (s1.length > s2.length) return false;
+
+  var base = 97;
+  var s1map = new Array(26).fill(0);
+  var s2map = new Array(26).fill(0);
+  for (var i = 0; i < s1.length; i++) {
+    s1map[s1.charCodeAt(i) - base]++;
+    s2map[s2.charCodeAt(i) - base]++;
+  }
+  var count = 0;
+  for (var i = 0; i < 26; i++) {
+    if (s1map[i] == s2map[i]) count++;
+  }
+  for (var i = 0; i < s2.length - s1.length; i++) {
+    var r = s2.charCodeAt(i + s1.length) - base;
+    var l = s2.charCodeAt(i) - base;
+    if (count == 26) return true;
+    s2map[r]++;
+    if (s2map[r] == s1map[r]) count++;
+    else if (s2map[r] == s1map[r] + 1) count--;
+    s2map[l]--;
+    if (s2map[l] == s1map[l]) count++;
+    else if (s2map[l] == s1map[l] - 1) count--;
+  }
+  return count == 26;
+};
+
+/*
+103 / 103 test cases passed.
+Status: Accepted
+Runtime: 64 ms
+Memory Usage: 38 MB
+*/

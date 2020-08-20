b1d179d5244b05433552588daed67d2b17849173
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-08-20 14:48:21 +0500

    May 17th

--- Week 3/May17th.js
+++ Week 3/May17th.js
@@ -48,3 +48,45 @@ Status: Accepted
 Runtime: 160 ms
 Memory Usage: 43 MB
 */
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function (s, p) {
+  if (s === p) return [0];
+
+  var res = [];
+  var pCount = Array(26).fill(0);
+  var sCount = Array(26).fill(0);
+  var base = "a".charCodeAt(0);
+
+  for (var i = 0; i < p.length; i++) {
+    pCount[p.charCodeAt(i) - base]++;
+  }
+
+  for (var i = 0; i < s.length; i++) {
+    sCount[s.charCodeAt(i) - base]++;
+    if (i >= p.length) {
+      sCount[s.charCodeAt(i - p.length) - base]--;
+    }
+    if (equal()) res.push(i - p.length + 1);
+  }
+
+  function equal() {
+    for (var i = 0; i < 26; i++) {
+      if (pCount[i] !== sCount[i]) return false;
+    }
+    return true;
+  }
+
+  return res;
+};
+
+/*
+36 / 36 test cases passed.
+Status: Accepted
+Runtime: 80 ms
+Memory Usage: 39.9 MB
+*/

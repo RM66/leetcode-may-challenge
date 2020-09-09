8e6b66384ed6ecd272ebc8c9d58428db6002f97a
Роман Ямалетдинов <iamaletdinov.rm@pik.ru>
2020-09-09 19:41:52 +0500

    May 20th

--- Week 3/May20th.js
+++ Week 3/May20th.js
@@ -33,3 +33,37 @@ Status: Accepted
 Runtime: 64 ms
 Memory Usage: 39.3 MB
 */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  var stack = [];
+  var node = root;
+  while (node) {
+    while (node) {
+      stack.push(node);
+      node = node.left;
+    }
+    node = stack.pop();
+    if (--k === 0) return node.val;
+    node = node.right;
+  }
+};
+
+/*
+91 / 91 test cases passed.
+Status: Accepted
+Runtime: 64 ms
+Memory Usage: 42.4 MB
+*/
